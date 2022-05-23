c1ae75b541f24a158b3e00e7dd95b5b3b6c4f8f9
Nisal-Sashmitha <IT20081034@my.sliit.lk>
2022-05-23 07:08:31 +0530

    creted posts without db connection

--- src/App.js
+++ src/App.js
@@ -1,10 +1,59 @@
 
 import './App.css';
+import Post from './Components/Post'
+import { useState } from 'react';
 
 function App() {
+  const [posts, setPosts] = useState([
+    {
+        username:"nisal123",
+        caption:"wow it worked!",
+        imageUrl: 'https://picsum.photos/200'
+     },
+    {   username:"pavi123",
+        caption:"beauty of the nature!",
+        imageUrl: 'https://picsum.photos/200'
+
+    },
+    {   username:"kasun123",
+        caption:"nice!",
+        imageUrl: 'https://picsum.photos/200'
+
+    }
+]);
   return (
     <div className="App">
-      <h1>hellow nisal!</h1>
+      <div className="app__header">
+        <img 
+          className="app__headerImage"
+          src="https://www.vectorlogo.zone/logos/instagram/instagram-wordmark.svg"
+          height="40px;"
+          
+          alt=""
+        />  
+      </div>
+      
+      {
+        posts.map(post =>(
+          <Post username={post.username} caption={post.caption} imageUrl={post.imageUrl}/>
+        )
+
+        )
+       }
+
+      <Post username='nisal123'
+          caption="Wow it worked!"
+          imageUrl='https://picsum.photos/200'/>
+      <Post username='pavi123'
+          caption="beauty of the nature!"
+          imageUrl='https://picsum.photos/200'/>
+
+      <Post username='kasun 12'
+          caption="noice noice!"
+          imageUrl='https://picsum.photos/200'/>
+      {/*header*/}
+      {/*post*/}
+      {/*post */}
     </div>
   );
 }
