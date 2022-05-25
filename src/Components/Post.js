3707a7fb9e09654afd16dae80cd8b0b78d142f8e
Nisal-Sashmitha <IT20081034@my.sliit.lk>
2022-05-26 01:06:46 +0530

    completed basics

--- src/Components/Post.js
+++ src/Components/Post.js
@@ -1,19 +1,48 @@
 import './../Css/Post.css'
 import Avatar from "@material-ui/core/Avatar";
 import { useEffect, useState } from 'react';
-import { getDoc, getFirestore,doc } from 'firebase/firestore';
+import { getFirestore,collection,onSnapshot,addDoc, serverTimestamp ,query,orderBy} from 'firebase/firestore';
+import { getAuth } from "firebase/auth";
 
 function Post({username, caption, imageUrl,postID}) {
     const [comments,setComments] = useState([]);
-    const db = getFirestore();
+    const [comment, setComment] = useState('');
+    const auth = getAuth();
+    const user = auth.currentUser;
+
+    
     useEffect(()=>{
+      let unsubscribe;
       if(postID) {
-        const docRef = doc(db,'posts',postID);
-        getDoc(docRef).then((doc)=>{
-          console.log(doc);
-        })
+        const collectionRef = collection(getFirestore(),"posts",postID,"comments");
+        const q = query(collectionRef,orderBy("timestamp","desc"))
+        unsubscribe = onSnapshot(q,(snapshot)=>{
+            setComments(snapshot.docs.map(doc =>doc.data()
+            ))
+        console.log(comments);
+        
+        });
+        
       }
-    })
+
+      return ()=>{
+        unsubscribe();
+      };
+
+      
+      
+    },[postID]);
+
+    const postComment =(event)=>{
+      event.preventDefault();
+      const collectionRef = collection(getFirestore(),"posts",postID,"comments");
+      const docRef = addDoc(collectionRef, {
+        text:comment,
+        username:user.displayName,
+        timestamp:serverTimestamp()
+      });
+      setComment('');
+    }
   
   return (
     <div className='post'>
@@ -35,6 +64,32 @@ function Post({username, caption, imageUrl,postID}) {
       {/*image */}
       <h4 className='post__text'><strong>{username} </strong>{caption}</h4>
       {/*username + caption */}
+      <div className='post__comments'>
+        {comments.map((comment)=>(
+          <p>
+            <strong>{comment.username} </strong>{comment.text}
+          </p>
+        ))}
+
+      </div>
+
+      {user && (<form className='post__commentBox'>
+        <input 
+          className='post__input'
+          type = "text"
+          placeholder='Add a comment...'
+          value={comment}
+          onChange={(e)=>setComment(e.target.value)}
+          />
+          <button 
+            className='post__button'
+            disabled={!comment}
+            type="submit"
+            onClick={postComment}
+          >Post</button>
+      </form>)}
+  
+      
 
     </div>
   )
